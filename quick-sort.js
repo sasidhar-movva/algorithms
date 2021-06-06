68674384bdf162029020d472ede3e4a46dc459cc
Sasidhar Movva <33686271+sasidhar-movva@users.noreply.github.com>
2021-06-06 21:12:04 +0530

    Update quick-sort.js

--- quick-sort.js
+++ quick-sort.js
@@ -1,1 +1,26 @@
+const input = [12, 11, 13, 5, 6, 7];
+quickSort(input, 0, input.length - 1) console.log(input);
 
+function quickSort(array, low, high) {
+    pivotIndex = high;
+    let i = low,
+        j = low;
+    if (low < high) {
+        while (j < high) {
+            if (array[j] < array[pivotIndex]) {
+                let temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+            }
+            j++;
+        }
+        if (j === high) {
+            let temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        quickSort(array, low, i - 1);
+        quickSort(array, i + 1, high);
+    }
+}
