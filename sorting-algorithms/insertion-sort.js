c29c1c0319c80bc4607a49c5462893121de10614
Sasidhar Movva <33686271+sasidhar-movva@users.noreply.github.com>
2021-06-07 11:55:54 +0530

    Update insertion-sort.js

--- sorting-algorithms/insertion-sort.js
+++ sorting-algorithms/insertion-sort.js
@@ -1,1 +1,26 @@
+const input = [12, 11, 13, 5, 6, 7];
 
+insertionSort(input, 1)
+
+console.log(input);
+
+function insertionSort(input, currentIndex) {
+  if (currentIndex === input.length) {
+    return;
+  }
+
+  if (input[currentIndex] > input[currentIndex - 1]) {
+    insertionSort(input, currentIndex + 1);;
+  }
+
+  let temp = input[currentIndex];
+  let j = currentIndex-1;
+
+  while (j >= 0 && temp < input[j]) {
+    input[j+1] = input[j];
+    j--;
+  }
+  input[j+1] = temp;
+
+  insertionSort(input, currentIndex + 1);
+}
