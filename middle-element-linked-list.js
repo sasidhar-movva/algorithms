d075d67e65add6ce0f2d37b2e501fe694de53fd9
Sasidhar Movva <33686271+sasidhar-movva@users.noreply.github.com>
2021-06-10 11:10:15 +0530

    Create middle-element-linked-list.js

---
+++ middle-element-linked-list.js
@@ -0,0 +1,61 @@
+//https://practice.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/1#
+let head; // head of linked list
+ 
+/* Linked list node */
+class Node {
+  constructor(val) {
+    this.data = val;
+    this.next = null;
+  }
+}
+
+for (i = 6; i > 0; --i) {
+  push(i);
+}
+
+const result = getMiddle(head);
+
+console.log(result);
+
+function push(new_data) {
+  /*
+   * 1 & 2: Allocate the Node & Put in the data
+   */
+  var new_node = new Node(new_data);
+
+  /* 3. Make next of new Node as head */
+  new_node.next = head;
+
+  /* 4. Move the head to point to new Node */
+  head = new_node;
+}
+
+// function getMiddleNodeData(slowNode, fastNode) {
+//   if (fastNode.next === undefined || fastNode.next.next === undefined) {
+//       console.log(slowNode.data);
+//       return slowNode.data;
+//   }
+  
+//   getMiddleNodeData(slowNode.next, fastNode.next.next);
+// }
+
+/* Should return data of middle node. If linked list is empty, then  -1*/
+function getMiddle(node)
+{ 
+  if (!node) {
+   return -1;
+  }
+  let slowNode = head;
+  let fastNode = head;
+  
+  while (fastNode.next !== undefined && fastNode.next.next !== undefined) {
+    slowNode = slowNode.next;
+    fastNode = fastNode.next.next;
+  }
+
+  if (fastNode.next !== undefined) {
+    return slowNode.next.data;
+  } else {
+    return slowNode.data;
+  }
+}
