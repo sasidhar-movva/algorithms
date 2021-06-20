b68a98b6acbd970ee27bae06987a9bf1be3d855a
Sasidhar Movva <33686271+sasidhar-movva@users.noreply.github.com>
2021-06-20 12:25:06 +0530

    Create binary-tree-to-double-linked-list.js

---
+++ tree/binary-tree-to-double-linked-list.js
@@ -0,0 +1,83 @@
+//https://practice.geeksforgeeks.org/problems/binary-tree-to-dll/1#
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Dll {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+let root = new Node(10);
+root.left = new Node(12);
+root.right = new Node(15);
+root.left.left = new Node(25);
+root.left.right = new Node(30);
+root.right.left = new Node(36);
+let head;
+let previous = null;
+
+const result = bToDLL(root);
+console.log('testing');
+
+function bToDLL(root) {
+    let dll = new Dll();
+
+    if (root === null) {
+        return dll;
+    }
+
+    processTree(root);
+
+    return head;
+}
+
+function processTree(root) {
+    if (root === null || root.data === null) return;
+
+    if (root.left !== null) {
+        processTree(root.left);
+    }
+
+    if (previous === null) {
+        head = root;
+    } else {
+        previous.right = root;
+        root.left = previous;
+    }
+
+    previous = root;
+
+    if (root.right) {
+        processTree(root.right);
+    }
+
+    return head;
+}
+
+function processRightTree(node) {
+    if (node === null) return;
+
+    let dll = new Dll(node.data);
+    tail.right = dll;
+    dll.left = tail;
+    tail = dll;
+
+    if (node.left) {
+        processLeftTree(node.left);
+    }
+
+    if (node.right) {
+        processRightTree(node.right);
+    }
+
+    return tail;
+}
