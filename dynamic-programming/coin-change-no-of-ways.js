3cd0adfd9fd066a71c92447523af87e45601e694
Sasidhar Movva <33686271+sasidhar-movva@users.noreply.github.com>
2021-09-04 10:55:15 +0530

    Create coin-change-no-of-ways.js

---
+++ dynamic-programming/coin-change-no-of-ways.js
@@ -0,0 +1,21 @@
+//https://practice.geeksforgeeks.org/problems/coin-change2448/1
+
+let coins = [1,2,3];
+let m = coins.length;
+let n = 4;
+
+var result = countNoOfWays(coins, m, n);
+console.log(result);
+
+function countNoOfWays(coins, m, n) {
+  let table = new Array(n+1).fill(0);
+  table[0] = 1;
+
+  for (let index = 0; index < m; index++) {
+    for (let j = coins[index]; j<= n; j++) {
+      table[j] += table[j-coins[index]];
+    }
+  }
+
+  return table[n];
+}
